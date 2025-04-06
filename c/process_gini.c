123de3ef8afeb6a79408bd012d277c5f8510fa32
mateooD <mateo99.d@gmail.com>
2025-04-06 18:45:19 -0300

    Avance en integracion Python y C

---
+++ c/process_gini.c
@@ -0,0 +1,6 @@
+#include "process_gini.h"
+
+int convertir_y_sumar(float valor) {
+    int entero = (int)valor;
+    return entero + 1;
+}
---
+++ c/process_gini.h
@@ -0,0 +1,6 @@
+#ifndef PROCESS_GINI_H
+#define PROCESS_GINI_H
+
+int convertir_y_sumar(float valor);
+
+#endif

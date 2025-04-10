25f84227bbd7e48693c034984ec97d23bf7f7727
constanzamedran <constanza.medran@mi.unc.edu.ar>
2025-04-10 12:19:34 -0300

    1era iteracion

--- c/process_gini.c
+++ c/process_gini.c
@@ -1,6 +1,13 @@
 #include "process_gini.h"
 
-int convertir_y_sumar(float valor) {
-    int entero = (int)valor;
-    return entero + 1;
+int* convertir_y_sumar(float *arr, int size) {
+    int* array_int = malloc(size * sizeof(int));
+    for(int i=0;i<size;i++){
+        array_int[i]=(int)arr[i];
+        array_int[i]++;
+    }
+    return array_int;
+}
+void free_memory(int *arr){
+    free(arr);
 }
--- c/process_gini.h
+++ c/process_gini.h
@@ -1,6 +1,15 @@
 #ifndef PROCESS_GINI_H
 #define PROCESS_GINI_H
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-int convertir_y_sumar(float valor);
+/*
+    Esta funcion transforma de tipo float a tipo int y le suma 1 
+    a cada elemento de la array proveniente del codigo en python 
+    @param arr :arreglo con los indices gini
+    @param size :tamaño del arreglo
+*/
+int* convertir_y_sumar(float *arr, int size);
 
 #endif

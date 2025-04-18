a175b856c3228787e601b7b877d6155cfbe2bc7a
Tomas Martin <tomas.martin@unc.edu.ar>
2025-04-18 16:52:22 -0300

    cambios 64 bits para que funcione igual a 32 bits

--- c/process_gini_asm64.s
+++ c/process_gini_asm64.s
@@ -1,12 +1,15 @@
 section .text
-    global convertir_y_sumar_asm   ; Hacemos pública la función
+    global convertir_y_sumar_asm
 
 convertir_y_sumar_asm:
-    ; Recibimos un número float en xmm0 (pasado desde C)
-    ; Convertimos de float a entero truncado y le sumamos 1
-    cvttss2si rax, xmm0         ; Convertir de float a entero (en rax)
-    add     rax, 1              ; Sumar 1 al valor entero
-    ret                         ; Devolver el resultado (en rax)
- 
+    ; Redondear el valor float en xmm0 al entero más cercano
+    roundss xmm0, xmm0, 0b00000000     ; modo 0: round to nearest
 
+    ; Convertir el valor ya redondeado a entero
+    cvttss2si rax, xmm0
+
+    ; Sumar 1
+    add rax, 1
+
+    ret
    
